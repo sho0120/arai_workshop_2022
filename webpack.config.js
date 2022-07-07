6ec6011e5e91f1705300c02d22fc56f0363bae81
arco0922 <k9o2u2ta@icloud.com>
2022-07-08 01:51:22 +0900

    initial commit for firebase hosting

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+const envConfig = require("dotenv").config({
+	path: path.resolve(__dirname, `./.env`),
+});
+
+module.exports = {
+	mode: "production",
+	entry: "./src/index.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+	},
+
+	plugins: [
+		new webpack.DefinePlugin({
+			"process.env": JSON.stringify({
+				...envConfig.parsed,
+			}),
+		}),
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "./src/index.html"),
+		}),
+	],
+	devServer: {
+		static: {
+			directory: path.join(__dirname, "src"),
+		},
+		port: 3000,
+		compress: true,
+		hot: true,
+		open: true,
+		historyApiFallback: true,
+	},
+};
